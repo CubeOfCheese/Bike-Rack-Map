1e89ce69a359318578b88d7d89bb44e8b87e9984
CubeOfCheese <cubeofcheese@gmail.com>
2019-08-19 23:37:48 -0700

    Add and delete markers, infoboxes

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Bike-Rack-Map
 A crowd sourced map for available bike parking to help bicyclists find a place to lock their bikes.
+
+Things to do:
+<!-- 1. Add functionality to add and delete racks -->
+<!-- 2. Add functionality to display and detail bike rack capacity -->
+3. Add Legend
+4. Add functionality to edit racks
+5. Add donation jar
+6. Add about me (Billy T.) and contact me
+7. Prettify with help from Maya <Due Wednesday>
+8. Publish on Heroku? <Due Thursday>
+9. Send link to SABA, City, gettingaroundsac blog, facebook groups, Sac State, Sac City College map page
+(916) 278-6242
+sustainability@csus.edu
+
+v2
+1. Look into using Geofire so that only data around user is loaded
--- firebasemap.js
+++ firebasemap.js
@@ -1,11 +1,13 @@
 /**
  * Data object to be written to Firebase.
  */
-var data = {
+var data = {  // This is what is stored in Firebase
   sender: null,
   timestamp: null,
   lat: null,
-  lng: null
+  lng: null,
+  latlng: null,
+  capacity: null
 };
 var config = {
   apiKey: 'AIzaSyCIMOtd7tORwU0Ssx8atqy7wegaqRtBmyw',
@@ -15,13 +17,14 @@ var config = {
   storageBucket: 'bike-rack-map.appspot.com',
   messagingSenderId: '285779793579'
 };
-
+var infoWindows = []; //Used to access all open infowindows
 firebase.initializeApp(config);
 
-//   var marker = new google.maps.Marker({position: uluru, map: map});
 var map;
+var markers
 var transitLayer;
 var bikeLayer;
+var infoWindow;
 
 var bikePathCheckBox = document.querySelector('#bike-paths');
 var lightRailCheckBox = document.querySelector('#light-rail');
@@ -43,10 +46,53 @@ lightRailCheckBox.onchange = function() {
   }
 }
 
+function deleteMarker() {
+
+  var latList = document.getElementsByClassName('marker-lat')
+  var lat = latList[latList.length-1].innerHTML.substring(4);
+  var lngList = document.getElementsByClassName('marker-lng')
+  var lng = lngList[lngList.length-1].innerHTML.substring(4);
+
+  console.log(lat+lng);
+  var clicks = firebase.database().ref('clicks');
+  var ref = clicks.orderByChild('latlng').equalTo(lat+lng).once("value").then(function(snapshot) {
+    snapshot.forEach(function(child) {
+      child.ref.remove();
+      console.log("Removed!");
+    })
+    // Remove old data from the heatmap when a point is removed from firebase.
+
+  });
+  clicks.on('child_removed', function(snapshot, prevChildKey) {
+    console.log("child removed");
+    var markersData = markers;
+    var i = 0;
+    console.log(markersData[0]);
+    while (snapshot.val().lat != markersData[i].position.lat() || snapshot.val().lng != markersData[i].position.lng()) {
+      i++;
+      console.log(i);
+    }
+    console.log(markersData);
+    console.log(i);
+    markers[i].setMap(null);
+    markers.splice(i, 1);
+    console.log(markers);
+  });
+
+  // .on('child_added', function(snapshot) {
+  //   console.log(snapshot);
+  //   // snapshot.remove();
+  // });
+
+  // console.log(markerToDelete);
+  // markerToDelete.remove();
+  // ref.remove();
+}
+
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 38.576488, lng: -121.493239},
-    zoom: 10,
+    zoom: 17,
     styles: [
       {
         "featureType": "road.highway",
@@ -77,21 +123,62 @@ function initMap() {
   // infoBoxDiv.index = 1;
   // map.controls[google.maps.ControlPosition.TOP_CENTER].push(infoBoxDiv);
   // Create a heatmap.
-  var heatmap = new google.maps.visualization.HeatmapLayer({
-    data: [],
-    map: map,
-    radius: 16
-  });
-  map.addListener('click', function(e) {
+  markers = [];
+
+  var centerMarkerIcon = document.createElement("IMG");
+  centerMarkerIcon.src = 'marker-icon.png';
+  var centerMarker = document.createElement('div');
+  centerMarker.append(centerMarkerIcon);
+  centerMarker.setAttribute("class", "center-marker")
+  map.getDiv().append(centerMarker);
+  console.log(map.getDiv());
+
+  map.addListener('dblclick', function(e) {
     data.lat = e.latLng.lat();
     data.lng = e.latLng.lng();
+    data.latlng = data.lat.toString() + data.lng.toString();
+    data.capacity = 5;
     addToFirebase(data);
   });
+
+  map.addListener('click', function(e) {
+    for (i=0; i<infoWindows.length; i++) {
+      infoWindows[i].close();
+    }
+  });
+
   transitLayer = new google.maps.TransitLayer();
   transitLayer.setMap(map);
   bikeLayer = new google.maps.BicyclingLayer();
   bikeLayer.setMap(map);
-  initAuthentication(initFirebase.bind(undefined, heatmap));
+
+  initAuthentication(initFirebase.bind(undefined, markers));
+  infoWindow = new google.maps.InfoWindow;
+  if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(function(position) {
+    var pos = {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude
+    };
+
+    infoWindow.setPosition(pos);
+    infoWindow.setContent('Location found.');
+    infoWindow.open(map);
+    map.setCenter(pos);
+  }, function() {
+    handleLocationError(true, infoWindow, map.getCenter());
+  });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.open(map);
 }
 function makeInfoBox(controlDiv, map) {
   // Set CSS for the control border.
@@ -125,7 +212,7 @@ function initAuthentication(onAuthSuccess) {
 
   firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
-      data.sender = user.uid;
+      data.sender = user.uid; // Adds sender to firebase data
       onAuthSuccess();
     } else {
       // User is signed out.
@@ -162,47 +249,61 @@ function getTimestamp(addClick) {
     }
   });
 }
-// /**
-//  * Set up a Firebase with deletion on clicks older than expirySeconds
-//  * @param {!google.maps.visualization.HeatmapLayer} heatmap The heatmap to
-//  * which points are added from Firebase.
-//  */
-function initFirebase(heatmap) {
-  // 10 minutes before current time.
-  var startTime = new Date().getTime() - (60 * 10 * 1000);
 
+function initFirebase(markers) {
   // Reference to the clicks in Firebase.
   var clicks = firebase.database().ref('clicks');
+  console.log(clicks);
 
-  // Listen for clicks and add them to the heatmap.
-  clicks.orderByChild('timestamp').startAt(startTime).on('child_added',
+  // Listen for clicks and add them to the map.
+  clicks.orderByChild('timestamp').on('child_added',
     function(snapshot) {
       // Get that click from firebase.
       var newPosition = snapshot.val();
+      console.log(newPosition);
       var point = new google.maps.LatLng(newPosition.lat, newPosition.lng);
-      var elapsedMs = Date.now() - newPosition.timestamp;
 
-      // Add the point to the heatmap.
-      heatmap.getData().push(point);
+      // Add the point to the map.
+      // markers.push(point);
+      console.log(markers.length);
+      var newMarker = new google.maps.Marker({
+          map: map,
+          position: point,
+      });
+      markers.push(newMarker);
+      console.log(newMarker.position.lat());
+      var capacityInfo = "Capacity: " + newPosition.capacity;
+      console.log(newPosition);
+      var deleteLat = newMarker.position.lat();
+      var deleteLng = newMarker.position.lng();
+      var contentString = '<div id="content">'+
+            '<div id="siteNotice">'+
+            '</div>'+
+            '<div id="bodyContent">'+
+            capacityInfo+
+            '<p class="marker-lat">Lat:' + newMarker.position.lat()+ '</p>'+
+            '<p class="marker-lng">Lng:' + newMarker.position.lng()+ '</p>'+
+            '<br>'+
+            '<button onClick="">Edit Rack</button>'+
+            '<button onClick="deleteMarker()">Delete Rack</button>'+
+            '</div>'+
+            '</div>';
 
-      // Request entries older than expiry time (10 minutes).
-      var expiryMs = Math.max(60 * 10 * 1000 - elapsedMs, 0);
-      // Set client timeout to remove the point after a certain time.
-      window.setTimeout(function() {
-        // Delete the old point from the database.
-        snapshot.ref.remove();
-      }, expiryMs);
+      var infowindow = new google.maps.InfoWindow({
+        content: contentString
+      });
+
+      newMarker.addListener('click', function() {
+        infoWindows.push(infowindow);
+        infowindow.open(map, newMarker);
+      });
+      // // Request entries older than expiry time (10 minutes).
+      // var expiryMs = Math.max(60 * 10 * 1000 - elapsedMs, 0);
+      // // Set client timeout to remove the point after a certain time.
+      // window.setTimeout(function() {
+      //   // Delete the old point from the database.
+      //   snapshot.ref.remove();
+      // }, expiryMs);
     }
   );
-
-  // Remove old data from the heatmap when a point is removed from firebase.
-  clicks.on('child_removed', function(snapshot, prevChildKey) {
-    var heatmapData = heatmap.getData();
-    var i = 0;
-    while (snapshot.val().lat != heatmapData.getAt(i).lat()
-      || snapshot.val().lng != heatmapData.getAt(i).lng()) {
-      i++;
-    }
-    heatmapData.removeAt(i);
-  });
 }
